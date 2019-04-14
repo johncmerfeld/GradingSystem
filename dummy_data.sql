d2fd769a719cb10fad70e9e3bd7ce62de75d1c1f
John Merfeld <john.merfeld@gmail.com>
2019-04-14 14:09:05 -0400

    loaded tables with dummy data

---
+++ dummy_data.sql
@@ -0,0 +1,47 @@
+LOAD DATA INFILE "dummy_data/studenttype.csv"
+INTO TABLE StudentType
+FIELDS TERMINATED BY ","
+ENCLOSED BY '"'
+LINES TERMINATED BY "\n";
+
+LOAD DATA INFILE "dummy_data/scoringmethod.csv"
+INTO TABLE ScoringMethod
+FIELDS TERMINATED BY ","
+ENCLOSED BY '"'
+LINES TERMINATED BY "\n";
+
+LOAD DATA INFILE "dummy_data/course.csv"
+INTO TABLE Course 
+FIELDS TERMINATED BY ","
+ENCLOSED BY '"'
+LINES TERMINATED BY "\n";
+
+LOAD DATA INFILE "dummy_data/student.csv"
+INTO TABLE Student
+FIELDS TERMINATED BY ","
+ENCLOSED BY '"'
+LINES TERMINATED BY "\n";
+
+LOAD DATA INFILE "dummy_data/enrolled.csv"
+INTO TABLE Enrolled
+FIELDS TERMINATED BY ","
+ENCLOSED BY '"'
+LINES TERMINATED BY "\n";
+
+LOAD DATA INFILE "dummy_data/category.csv"
+INTO TABLE Category
+FIELDS TERMINATED BY ","
+ENCLOSED BY '"'
+LINES TERMINATED BY "\n";
+
+LOAD DATA INFILE "dummy_data/gradeditem.csv"
+INTO TABLE GradedItem
+FIELDS TERMINATED BY ","
+ENCLOSED BY '"'
+LINES TERMINATED BY "\n";
+
+LOAD DATA INFILE "dummy_data/studentgrade.csv"
+INTO TABLE StudentGrade
+FIELDS TERMINATED BY ","
+ENCLOSED BY '"'
+LINES TERMINATED BY "\n";
--- init.sql
+++ init.sql
@@ -2,18 +2,23 @@ CREATE DATABASE gradingsystem;
 USE gradingsystem;
 
 CREATE TABLE Course(
-	courseId INT PRIMARY KEY,
-	courseName VARCHAR(50) NOT NULL,
-	semester VARCHAR(30) NOT NULL,
+	courseId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	courseName VARCHAR(50),
+	semester VARCHAR(30),
 	isActive BOOLEAN);
 
+CREATE TABLE StudentType(
+        studentTypeId INT PRIMARY KEY,
+        studentType VARCHAR(30));
+
 CREATE TABLE Student(
-	studentId INT PRIMARY KEY, -- BUID
+	studentId INT NOT NULL PRIMARY KEY, -- BUID
 	firstName VARCHAR(30),
 	middleInitial CHAR,
 	lastName VARCHAR(30),
 	email VARCHAR(30),
-	isGradStudent BOOLEAN);
+	studentType INT,
+	FOREIGN KEY (studentType) REFERENCES StudentType(studentTypeId) );
 
 CREATE TABLE Enrolled(
 	courseId INT,
@@ -23,21 +28,25 @@ CREATE TABLE Enrolled(
 	FOREIGN KEY (studentId) REFERENCES Student(studentId) );
 
 CREATE TABLE Category(
-	categoryId INT,
+	categoryId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	categoryName VARCHAR(30),
 	courseId INT,
 	percentageWeight FLOAT,
 	FOREIGN KEY (courseId) REFERENCES Course(courseId) );
-	
 
-CREATE TABLE GradedItems(
-	gradedItemId INT,
+CREATE TABLE ScoringMethod(
+        scoringMethodId INT PRIMARY KEY,
+        scoringMethod VARCHAR(30));
+
+CREATE TABLE GradedItem(
+	gradedItemId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	gradedItemName VARCHAR(30),
 	categoryId INT,
 	maxPoints FLOAT,
-	deductionScoring BOOLEAN,
+	scoringMethodId INT,
 	percentageWeight FLOAT,
-	FOREIGN KEY (categoryId) REFERENCES Category(categoryId) );
+	FOREIGN KEY (categoryId) REFERENCES Category(categoryId),
+	FOREIGN KEY (scoringMethodId) REFERENCES ScoringMethod(scoringMethodId) );
 
 CREATE TABLE StudentGrade(
 	studentId INT,
