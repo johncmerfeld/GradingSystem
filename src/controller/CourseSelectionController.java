547ec19684652568253385d63732516d2b18ee07
Abhimanyu Jain <abhimanyujain2k6@gmail.com>
2019-04-21 15:50:30 -0400

    constructor added for CourseSelectionController

--- src/controller/CourseSelectionController.java
+++ src/controller/CourseSelectionController.java
@@ -5,6 +5,10 @@ import model.Course;
 import model.Database;
 
 public class CourseSelectionController implements CourseSelection{
+	
+	public CourseSelectionController()
+	{}
+	
 	@Override
 	public ArrayList<Course> getAllCourses() {
 		ArrayList<Course> listOfCourses= Database.getAllCourses();
