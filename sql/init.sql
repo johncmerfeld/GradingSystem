6294e9fec78dc634a6d315862a33dd4d8e1b2f95
John Merfeld <john.merfeld@gmail.com>
2019-04-25 21:54:43 -0400

    making middle name a string

--- sql/init.sql
+++ sql/init.sql
@@ -14,7 +14,7 @@ CREATE TABLE StudentType(
 CREATE TABLE Student(
 	studentId INT NOT NULL PRIMARY KEY, -- BUID
 	firstName VARCHAR(30),
-	middleInitial CHAR,
+	middleInitial VARCHAR(30),
 	lastName VARCHAR(30),
 	email VARCHAR(30),
 	studentType INT,
