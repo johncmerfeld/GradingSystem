bf4472d7174f38ba6b29af553cfd636acdd3cf36
chi zhang <zhang.chi1@husky.neu.edu>
2019-04-27 00:46:30 -0400

    updated tests

--- src/controller/CourseworkSummaryControllerTest.java
+++ src/controller/CourseworkSummaryControllerTest.java
@@ -8,8 +8,8 @@ public class CourseworkSummaryControllerTest {
 		CourseworkSummaryController c1 = new CourseworkSummaryController(1);
 		System.out.println(c1.getCourse(1).getCourseName());
 		System.out.println(c1.getCourse(1).getCourseSemester());
-//		String[][] matrix = c1.getStudentDataIn2dArray(1);
-//		System.out.println(matrix[0]);
+		String[][] matrix = c1.getStudentDataIn2dArray(1);
+		System.out.println(matrix[0]);
 		String BU_Id = "U123";
 		String intermediate = BU_Id.substring(1);
 		int bu_id = Integer.parseInt(intermediate);
